0ba66570191b30ec3a1cf98dacb3c0535dee9ec2
Erin ODea <odea.erin@gmail.com>
2022-04-11 14:57:29 -0500

    set card for tarot

--- myapp/tarot_posts/forms.py
+++ myapp/tarot_posts/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 class TarotPostForm(FlaskForm):
     title = StringField('Recipient/Recipients', validators=[DataRequired()])
-    spread = TextAreaField('Spread', validators=[DataRequired()])
+    spread = StringField('Spread', validators=[DataRequired()])
     card1 = StringField('First Card', validators=[DataRequired()])
     card2 = StringField('Second Card', validators=[DataRequired()])
     card3 = StringField('Third Card', validators=[DataRequired()])
