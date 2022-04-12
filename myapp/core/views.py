598f3515b9c19dedf2736ab566e88f98f7be9f98
Erin ODea <odea.erin@gmail.com>
2022-04-12 11:07:40 -0500

    change styling entirely

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -6,7 +6,7 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
-    tarot_posts = TarotPost.query.order_by(TarotPost.date.desc()).paginate(page=page, per_page=5)
+    tarot_posts = TarotPost.query.order_by(TarotPost.date.desc()).paginate(page=page, per_page=8)
     return render_template('index.html', tarot_posts=tarot_posts)
 
 @core.route('/info')
